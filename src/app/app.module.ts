bc1a8dec07cc8536255a1180606a62479b15a939
Yahia Refaiea <yahiarefaiea@gmail.com>
2023-09-17 16:24:12 +0800

    add basic design and layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxsModule } from '@ngxs/store';
 import { NgxsDevtoolsModule } from '@ngxs/devtools-plugin';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     NgxsModule.forRoot([]),
     NgxsDevtoolsModule.forRoot(),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
