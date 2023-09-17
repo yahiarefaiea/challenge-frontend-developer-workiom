6c460b35beb8bf692e292719569cd47aebd6890e
Yahia Refaiea <yahiarefaiea@gmail.com>
2023-09-17 16:06:03 +0800

    add NGXS for state management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsDevtoolsModule } from '@ngxs/devtools-plugin';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxsModule.forRoot([]),
+    NgxsDevtoolsModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
