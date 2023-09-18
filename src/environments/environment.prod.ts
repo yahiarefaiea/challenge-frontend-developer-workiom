20354efe94ee6b57d6ca9b2de4512f80b4451bec
Yahia Refaiea <yahiarefaiea@gmail.com>
2023-09-18 23:14:46 +0800

    add env var

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  youtubeApiKey: 'AIzaSyBLWIHwCUL49fhN_gBu1y27XUc9t-xM458'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  youtubeApiKey: 'AIzaSyBLWIHwCUL49fhN_gBu1y27XUc9t-xM458'
 };
 
 /*
