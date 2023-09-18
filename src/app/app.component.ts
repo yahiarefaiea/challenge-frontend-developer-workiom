34679113c339f5f133d3edd35816efd77a413fb0
Yahia Refaiea <yahiarefaiea@gmail.com>
2023-09-18 18:17:12 +0800

    finalize root files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Store } from '@ngxs/store';
+import { Video } from './core/state/app.state.model';
+import { FetchVideos, UpdateVideoOrder } from './core/state/app.actions';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'challenge-frontend-developer-workiom';
+export class AppComponent implements OnDestroy {
+  channelId: string = '';
+  videos: Video[] = [];
+  error: string | null = null;
+  private subscription: Subscription = new Subscription();
+
+  constructor(private store: Store) {
+    const videos$ = this.store.select(state => state.app.videos).subscribe(videos => {
+      this.videos = videos;
+    }, err => {
+      this.error = 'Failed to fetch videos';
+    });
+
+    this.subscription.add(videos$);
+  }
+
+  onSearch(): void {
+    if (this.channelId !== this.store.selectSnapshot(state => state.app.lastSearchedChannelId)) {
+      this.store.dispatch(new FetchVideos(this.channelId));
+    }
+  }
+
+  onRefresh(): void {
+    const currentChannelId = this.store.selectSnapshot(state => state.app.lastSearchedChannelId);
+    if (currentChannelId) {
+      this.store.dispatch(new FetchVideos(currentChannelId));
+    }
+  }
+
+  onDrop(event: CdkDragDrop<string[]>): void {
+    moveItemInArray(this.videos, event.previousIndex, event.currentIndex);
+    this.store.dispatch(new UpdateVideoOrder(this.videos));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { NgxsModule } from '@ngxs/store';
-import { FormsModule } from '@angular/forms';
+import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppState } from './core/state/app.state';
+import { VideoCardComponent } from './shared/components/video-card/video-card.component';
+import { SafeVideoUrlPipe } from './shared/components/video-card/video-url.pipe';
+import { HttpClientModule } from '@angular/common/http';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoCardComponent,
+    SafeVideoUrlPipe,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxsModule.forRoot([]),
+    DragDropModule,
+    NgxsModule.forRoot([AppState]),
+    NgxsStoragePluginModule.forRoot({ key: 'app' }),
     FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
