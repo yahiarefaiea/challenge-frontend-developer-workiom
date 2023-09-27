d27822cea3ba690a28bda9266d95e22cc9780d65
Yahia Refaiea <yahiarefaiea@gmail.com>
2023-09-27 09:49:27 +0800

    remove PageNotFoundComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
-const routes: Routes = [
-  { path: '**', component: PageNotFoundComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
