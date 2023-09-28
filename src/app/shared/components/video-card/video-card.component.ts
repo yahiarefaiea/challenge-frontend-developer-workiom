70d6aca03fd8e941968f361a9f4b81c122be27f5
Yahia Refaiea <yahiarefaiea@gmail.com>
2023-09-29 03:58:39 +0800

    animate overlay on hover

--- src/app/shared/components/video-card/video-card.component.ts
+++ src/app/shared/components/video-card/video-card.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
+import { gsap } from 'gsap';
 import { Store } from '@ngxs/store';
 import { UpdateVideoNote } from 'src/app/core/state/app.actions';
 import { Video } from 'src/app/core/state/app.state.model';
+
+const ease = 'power4.inOut'
+const duration = 0.5
 
 @Component({
   selector: 'app-video-card',
@@ -20,7 +24,7 @@ export class VideoCardComponent implements OnInit {
   videoUrl: string = '';
   thumbnailUrl: string = '';
 
-  constructor(private store: Store) {}
+  constructor(private store: Store, private el: ElementRef) {}
 
   ngOnInit(): void {
     this.videoUrl = 'https://www.youtube.com/embed/' + this.video.id;
@@ -29,6 +33,18 @@ export class VideoCardComponent implements OnInit {
 
   toggleVideo(): void {
     this.showVideo = !this.showVideo;
+  }
+
+  showOverlay(): void {
+    const element = this.el.nativeElement;
+    const backgroundElement = element.querySelector('.video-card > .overlay');
+    gsap.to(backgroundElement, { ease, duration, top: -14, right: -14, bottom: -14, left: -14, opacity: 1 });
+  }
+
+  hideOverlay(): void {
+    const element = this.el.nativeElement;
+    const backgroundElement = element.querySelector('.video-card > .overlay');
+    gsap.to(backgroundElement, { ease, duration, top: 14, right: 14, bottom: 14, left: 14, opacity: 0 });
   }
 
   onNoteChange(videoId: string, newNote: string): void {
