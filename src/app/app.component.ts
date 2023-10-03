67780bac4234c75b977a8ac514815177bcba1727
Yahia Refaiea <yahiarefaiea@gmail.com>
2023-10-03 16:15:09 +0800

    quick fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, ElementRef, NgZone, ChangeDetectorRef, ChangeDetectionStrategy, DoCheck } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef } from '@angular/core';
 import Muuri from 'muuri';
 import { Store } from '@ngxs/store';
 import { Video, AppStateModel } from './core/state/app.state.model';
@@ -15,8 +15,7 @@ import { isEqual } from 'lodash';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent implements OnInit, OnDestroy {
   channelId: string = '';
@@ -25,9 +24,8 @@ export class AppComponent implements OnInit, OnDestroy {
   dragStartIndex: number | null = null;
   error: string | null = null;
   private subscription: Subscription = new Subscription();
-  private previousVideos: Video[] = [];
 
-  constructor(private store: Store, private el: ElementRef, private ngZone: NgZone, private cdRef: ChangeDetectorRef) {
+  constructor(private store: Store, private el: ElementRef) {
     const videos$ = this.store.select((state: { app: AppStateModel }) => {
       const currentChannelId = state.app.lastSearchedChannelId;
       const currentChannel = currentChannelId ? findChannel(state.app.channels, currentChannelId) : undefined;
@@ -66,38 +64,36 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   ngAfterViewInit() {
-    setTimeout(() => {
-        if (this.videos.length !== 0) {
-            this.initializeMuuriGrid();
-        }
-    }, 100);
-  }
-
-  ngDoCheck() {
-    if (!isEqual(this.previousVideos, this.videos)) {
-      console.log('Videos changed!');
-      this.previousVideos = [...this.videos];
-      this.syncMuuriGrid();
-    }
-  }
-
-  private syncMuuriGrid(): void {
-    if (!this.muuriGrid) {
+    if (this.videos.length !== 0) {
       this.initializeMuuriGrid();
-    } else {
-      this.ngZone.runOutsideAngular(() => {
-        const newItems = Array.from(this.el.nativeElement.querySelectorAll('.muuri-item:not([data-muuri-added])'));
-        if (newItems.length > 0) {
-          newItems.forEach((item: any) => item.setAttribute('data-muuri-added', 'true'));
-          this.muuriGrid.add(newItems);
-        }
-
-        this.muuriGrid.refreshItems().layout(true);
-        // this.muuriGrid.refreshItems().layout().synchronize();
-        this.cdRef.detectChanges();
-      });
     }
   }
+
+  // ngDoCheck() {
+  //   if (!isEqual(this.previousVideos, this.videos)) {
+  //     console.log('Videos changed!');
+  //     this.previousVideos = [...this.videos];
+  //     this.syncMuuriGrid();
+  //   }
+  // }
+
+  // private syncMuuriGrid(): void {
+  //   if (!this.muuriGrid) {
+  //     this.initializeMuuriGrid();
+  //   } else {
+  //     this.ngZone.runOutsideAngular(() => {
+  //       const newItems = Array.from(this.el.nativeElement.querySelectorAll('.muuri-item:not([data-muuri-added])'));
+  //       if (newItems.length > 0) {
+  //         newItems.forEach((item: any) => item.setAttribute('data-muuri-added', 'true'));
+  //         this.muuriGrid.add(newItems);
+  //       }
+
+  //       this.muuriGrid.refreshItems().layout(true);
+  //       // this.muuriGrid.refreshItems().layout().synchronize();
+  //       this.cdRef.detectChanges();
+  //     });
+  //   }
+  // }
 
   private initializeMuuriGrid(): void {
     const element = this.el.nativeElement;
@@ -107,24 +103,24 @@ export class AppComponent implements OnInit, OnDestroy {
     //   this.muuriGrid.destroy();
     // }
 
-    this.muuriGrid = new Muuri(muuriGridElement, {
-      items: '.muuri-item',
-      dragEnabled: true,
-      dragSort: () => {
-        return [this.muuriGrid];
-      }
-    })
-    .on('dragStart', (item) => {
-      this.dragStartIndex = this.muuriGrid.getItems().indexOf(item);
-    })
-    .on('dragEnd', (item) => {
-      if (this.dragStartIndex !== null) {
-        const oldIndex = this.dragStartIndex;
-        const newIndex = this.muuriGrid.getItems().indexOf(item);
-        this.handleReorder(oldIndex, newIndex);
-      }
-      this.dragStartIndex = null;
-    });
+    // this.muuriGrid = new Muuri(muuriGridElement, {
+    //   items: '.muuri-item',
+    //   dragEnabled: true,
+    //   dragSort: () => {
+    //     return [this.muuriGrid];
+    //   }
+    // })
+    // .on('dragStart', (item) => {
+    //   this.dragStartIndex = this.muuriGrid.getItems().indexOf(item);
+    // })
+    // .on('dragEnd', (item) => {
+    //   if (this.dragStartIndex !== null) {
+    //     const oldIndex = this.dragStartIndex;
+    //     const newIndex = this.muuriGrid.getItems().indexOf(item);
+    //     this.handleReorder(oldIndex, newIndex);
+    //   }
+    //   this.dragStartIndex = null;
+    // });
   }
 
   private getLastSearchedChannelId(): string {
